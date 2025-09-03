cb064bc74508091936f688c3fe2f331d1f55f87f
Weston Weissert <134028416+Wigglycastle@users.noreply.github.com>
2025-09-03 14:25:36 -0700

    Add SolversLib and bring in old code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/OpModes/AutoOp/PloughAuto.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Main.OpModes.AutoOp;
+
+// RR-specific imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Config
+@Autonomous(name = "PLOUGH_AUTO", group = "Autonomous")
+public class PloughAuto extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
+
+
+
+
